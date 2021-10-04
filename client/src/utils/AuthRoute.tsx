d9cb74ed48d5bc2773ba9d0141efe56757cda457
Francesco Elia Foradori <francesco.elia.foradori@gmail.com>
2021-10-04 10:58:54 +0200

    refactor: client

---
+++ client/src/utils/AuthRoute.tsx
@@ -0,0 +1,12 @@
+import { Route, Redirect, RouteProps } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { State } from '../clientTypes';
+
+const AuthRoute : React.FC<RouteProps> = ({ component: Component, ...args }) => {
+  const userInfo = useSelector((state: State) => state.user.userInfo);
+
+  if (!Component) return null;
+  return <Route {...args} render={(props) => (userInfo ? <Component {...props} /> : <Redirect to="/login" />)} />;
+}
+
+export default AuthRoute;
