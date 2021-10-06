b736ef6d2c5facc0cddcd9d3534025dd95633985
Francesco Elia Foradori <francesco.elia.foradori@gmail.com>
2021-10-06 23:48:18 +0200

    cy working

--- client/cypress/integration/Login/Login.spec.js
+++ client/cypress/integration/Login/Login.spec.js
@@ -1,5 +1,8 @@
 describe('Login', function () {
-  it('Should complete Login', function() {
-    cy.visit("http://localhost:3000");
-  })
-})
+  it('Should complete Login', function () {
+    cy.visit('http://localhost:3000');
+    cy.findByLabelText(/Email Address/).type('michael.scott@dunder.co.uk');
+    cy.findByLabelText(/Password/).type('123456');
+    cy.findByRole('button', { name: 'Sign In' }).click();
+  });
+});
